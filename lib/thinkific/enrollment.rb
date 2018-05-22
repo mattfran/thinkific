8956aeec02c501ee9282f26426dea42a42a7c984
v p <piousbox@vs-MacBook-Pro.local>
2018-05-21 18:46:20 -0700

    enrollment

--- lib/thinkific/enrollment.rb
+++ lib/thinkific/enrollment.rb
@@ -2,26 +2,32 @@ require 'httparty'
 
 class Thinkific::Enrollment
 
-  # @TODO: herehere
-  def self.create delta={}
-  	body = {
-      :first_name => customer[:first_name],
-      :last_name => customer[:last_name],
-      :email => customer[:email],
-      :send_welcome_email => false # @TODO: should be true?
+  # @TODO: pass :activated_at ?
+  def self.create delta={}  	
+ 		raise 'not implemented - expecting user' if !delta[:user]
+    raise 'not implemented - expecting user[:id]' if !delta[:user][:id]
+    raise 'not implemented - expecting course' if !delta[:course]
+    raise 'not implemented - expecting course[:id]' if !delta[:course][:id]
+
+  	# user is id or email
+=begin
+    if !delta[:user][:id] && delta[:user][:email]
+      u = Thinkific::User.where( :email => delta[:user][:email])
+      delta[:user][:id] = u[:id]
+    end
+=end
+
+    body = {
+      course_id: delta[:course][:id],
+      user_id: delta[:user][:id],
+      activated_at: Time.now
   	}
-  	result = HTTParty.post "#{Thinkific::DOMAIN}/api/public/v1/users", 
+  	result = HTTParty.post "#{Thinkific::DOMAIN}/api/public/v1/enrollments", 
   	  headers: Thinkific.headers,
   	  body: body.to_json
   	rs = JSON.parse result.body
-  	if rs['errors']
-  		if rs['errors']['email'][0] == 'has already been taken'
-  			u = Thinkific::User.where( :email => customer[:email] )
-  			return u
-  		end
-	  else
-	  	return rs
-	  end
+    raise rs['errors'] if rs['errors']
+    return rs
   end
 
 end
